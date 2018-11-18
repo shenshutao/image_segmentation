b18177370dbe63fca776bdd138591c0e2abdd49f
SHUTAO SHEN <sstwood@gmail.com>
2018-11-18 12:02:53 +0800

    small change

--- main.py
+++ main.py
@@ -20,15 +20,15 @@ if __name__ == "__main__":
 
     # model = FCN.get_fcn8s_model(input_shape=(224, 224, 3), class_no=2)
     # model = FCN.get_fcn16s_model(input_shape=(224, 224, 3), class_no=2)
-    model = FCN.get_fcn32s_model(input_shape=(224, 224, 3), class_no=2)
-    # model = Unet.get_unet_model(input_shape=(224, 224, 3), class_no=2)
+    # model = FCN.get_fcn32s_model(input_shape=(224, 224, 3), class_no=2)
+    model = Unet.get_unet_model(input_shape=(224, 224, 3), class_no=2)
 
     # model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=[mean_iou(num_class=2)])
     model.compile(loss=categorical_crossentropy, optimizer='adam', metrics=[mean_iou(num_class=2)])
 
     model.summary()
 
-    checkpoint = ModelCheckpoint('fcn16s.h5', verbose=1, save_best_only=False, period=3)
+    checkpoint = ModelCheckpoint('unet.h5', verbose=1, save_best_only=False, period=3)
     tensor_board = TensorBoard(log_dir='log', histogram_freq=0, write_graph=True, write_grads=True, write_images=True)
     learning_rate_reduction = ReduceLROnPlateau(monitor='loss', patience=2, verbose=1, factor=0.5, min_lr=0.000001)
 
@@ -39,10 +39,10 @@ if __name__ == "__main__":
         callbacks=[checkpoint, tensor_board, learning_rate_reduction]
     )
 
-    model.save('fcn16s.h5')
+    model.save('unet.h5')
 
     print('Start Test')
-    model = load_model('fcn16s.h5', compile=False)
+    model = load_model('unet.h5', compile=False)
     # 取val集10张图片，测试一下效果
     val_gen = horse_gen.get_horse_generator(horse_path, 'val', 1, input_hw=(224, 224, 3), mask_hw=(224, 224, 2))
 
