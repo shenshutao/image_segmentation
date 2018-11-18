2c84596a10d95057e6a390becb54f08a1846ebc2
SHUTAO SHEN <sstwood@gmail.com>
2018-11-18 13:22:20 +0800

    update metrix

--- custom_metrics.py
+++ custom_metrics.py
@@ -3,9 +3,12 @@ import keras.backend as K
 
 
 # Define IoU metric
+# IOU = true_positive / (true_positive + false_positive + false_negative).
 def mean_iou(num_class:int):
     def mean_iou(y_true, y_pred):
-        score, up_opt = tf.metrics.mean_iou(y_true, y_pred, num_class)
+        y = tf.argmax(y_true, -1)
+        y_hat = tf.argmax(y_pred, -1)
+        score, up_opt = tf.metrics.mean_iou(y, y_hat, num_class)
         K.get_session().run(tf.local_variables_initializer())
         with tf.control_dependencies([up_opt]):
             score = tf.identity(score)
