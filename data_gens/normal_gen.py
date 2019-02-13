3dd0b45dc1b773803138af4f3d3ef026d6c052eb
SHUTAO SHEN <sstwood@gmail.com>
2019-02-13 21:40:45 +0800

    Add augmentation for generator
    Add augmentation for data generator

--- data_gens/normal_gen.py
+++ data_gens/normal_gen.py
@@ -1,13 +1,53 @@
-import numpy as np
 import random
-import os
+
+import numpy as np
 from PIL import Image
-from keras.utils.np_utils import to_categorical
+from imgaug import augmenters as iaa
+from tensorflow.python.keras.utils.np_utils import to_categorical
 
 
-# Horse dataset, download from http://www.msri.org/people/members/eranb/
+def augment_img_mask(image, mask, augment_param):
+    horizontal_flip = 0.5 * augment_param.get('horizontal_flip', 0)
+    vertical_flip = 0.5 * augment_param.get('vertical_flip', 0)
+    rotation_range = [i * augment_param.get('rotation_range', 0) for i in (-1, 1)]
+    brightness_range = [1 - augment_param.get('brightness_range', 0), 1 + augment_param.get('brightness_range', 0)]
+
+    # print(horizontal_flip, vertical_flip, rotation_range, brightness_range)
+
+    augment_img = iaa.Sequential([
+        iaa.OneOf([
+            iaa.Affine(rotate=0),
+            iaa.Affine(rotate=90),
+            iaa.Affine(rotate=180),
+            iaa.Affine(rotate=270),
+            iaa.Fliplr(horizontal_flip),
+            iaa.Flipud(vertical_flip),
+        ]),
+        # iaa.Multiply(brightness_range),
+        iaa.Affine(rotate=rotation_range)],
+        # iaa.Crop(percent=(.2, .5))],
+        random_order=True)
+
+    _aug = augment_img._to_deterministic()
+    image_aug = _aug.augment_image(image)
+    mask_aug = _aug.augment_image(mask)
+    return image_aug, mask_aug
+
+
 def get_normal_generator(image_list, mask_list, batch_size=2, input_hw=(224, 224, 3), mask_hw=(224, 224, 20),
-                         preprocess=True, shuffle=True):
+                         preprocess=True, shuffle=True, augment_param=None):
+    """
+    Prepare the data generator, include image pre process and image augmentation.
+    :param image_list:
+    :param mask_list:
+    :param batch_size:
+    :param input_hw:
+    :param mask_hw:
+    :param preprocess:
+    :param shuffle:
+    :param augment_param: dict, such as {'horizontal_flip':True, 'rotation_range':30}
+    :return:
+    """
     def norm(x):
         x = x / 127.5
         x -= 1
@@ -29,16 +69,27 @@ def get_normal_generator(image_list, mask_list, batch_size=2, input_hw=(224, 224
                 image = Image.open(image_list[id])
                 image = image.resize(input_hw[0:2], Image.NEAREST)
                 image_np = np.asarray(image, dtype=np.uint8)
+
                 if preprocess:
                     image_np = norm(image_np)
-                batch_images[batch_id] = image_np
+
+                if image_np.shape != input_hw:   # try to reshape
+                    image_np = np.stack((image_np,) * input_hw[2], axis=-1)
+
+                    if image_np.shape != input_hw:
+                        raise Exception('Image size wrong')
 
                 mask = Image.open(mask_list[id])
                 mask = mask.resize(mask_hw[0:2], Image.NEAREST)
                 mask_np = np.asarray(mask, dtype=np.uint8).copy()
-                mask_np[mask_np == 255] = 0
+
+                # mask_np[mask_np != 0] = 1
+                if augment_param:
+                    image_np, mask_np = augment_img_mask(image_np, mask_np, augment_param)
+
                 mask_np = to_categorical(mask_np, num_classes=mask_hw[2])
 
+                batch_images[batch_id] = image_np
                 batch_masks[batch_id] = mask_np
 
                 batch_id += 1
@@ -48,12 +99,4 @@ def get_normal_generator(image_list, mask_list, batch_size=2, input_hw=(224, 224
             except FileNotFoundError:
                 print('Image/mask not found, Ignore', image_list[id], mask_list[id])
 
-# np.set_printoptions(threshold=np.nan)
-# horse_path = 'C:/Users/AlphaCat/Desktop/image_segmentation/weizmann_horse_db'
-# horse_gen = get_horse_generator(horse_path, train_or_val='train', batch_size=2, input_hw=(32, 32, 3),
-#                                 mask_hw=(32, 32, 2),
-#                                 preprocess=True, shuffle=True)
-#
-# print(next(horse_gen))
-# print(next(horse_gen)[0].shape)
-# print(next(horse_gen)[1].shape)
+
